3773eff803937105aa57f52ff9f562587210be2f
ethorneloe <ethorneloe@gmail.com>
2025-04-07 14:31:56 +1000

    Fix prop name

--- infrastructure/azurerm-runbooks/main.tf
+++ infrastructure/azurerm-runbooks/main.tf
@@ -1,14 +1,14 @@
 data "azuread_client_config" "current" {}
 
 resource "azuread_application" "my_app" {
-  display_name = "MySampleApp"
+  display_name = "MySampleApp2"
 
   # Assign owners (the SP running Terraform, for example)
   owners = [data.azuread_client_config.current.object_id]
 }
 
 resource "azuread_service_principal" "my_sp" {
-  client_id = azuread_application.my_app.application_id
+  client_id = azuread_application.my_app.client_id
 
   owners = var.owner_ids
 }
