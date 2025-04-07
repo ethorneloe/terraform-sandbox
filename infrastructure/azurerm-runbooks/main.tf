2bcb6b8a190ea5e19b47180d764c988059fd4f2c
ethorneloe <ethorneloe@gmail.com>
2025-04-07 14:56:47 +1000

    Update main.tf

--- infrastructure/azurerm-runbooks/main.tf
+++ infrastructure/azurerm-runbooks/main.tf
@@ -4,11 +4,11 @@ resource "azuread_application" "my_app" {
   display_name = "MySampleApp2"
 
   # Assign owners (the SP running Terraform, for example)
-  owners = var.owner_ids
+  owners = [data.azuread_client_config.current.object_id, "55a1d6f3-896a-4707-9b82-e8013c4001d0"]
 }
 
 resource "azuread_service_principal" "my_sp" {
   client_id = azuread_application.my_app.client_id
 
-  owners = var.owner_ids
+  owners = [data.azuread_client_config.current.object_id, "55a1d6f3-896a-4707-9b82-e8013c4001d0"]
 }
